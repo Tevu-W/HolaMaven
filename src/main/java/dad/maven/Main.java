562443af9bcbf86ceeaba7183397b12541f2516f
Tevu-W <stevenam97@hotmail.com>
2019-09-18 11:03:07 +0000

    first commit

---
+++ src/main/java/dad/maven/Main.java
@@ -0,0 +1,13 @@
+package dad.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String invertido = StringUtils.reverse("Hola Maven!");
+		System.out.println(invertido);
+	}
+
+}
